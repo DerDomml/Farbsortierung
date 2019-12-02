6994d7dbe0d2521e5ad588a3bd28e0f4c5443072
DerDomml <dominik.wagner66@gmail.com>
2019-12-02 10:30:58 +0100

    Update schrittmotor.h
    added byte defines

--- inc/schrittmotor.h
+++ inc/schrittmotor.h
@@ -1,1 +1,15 @@
+/*******************************
+    Includes für Schrittmotor
+*******************************/
 
+#ifndef SCHRITTMOTOR_H
+
+#define SMOT_NODE_ID 0x15           //Node-ID
+#define SMOT_DI_BYTE 0x180          //Digital Input Byte
+#define SMOT_AI_BYTES 0x380         //Analog Input Byte, 0 Status Byte, 1 K1_LB, 2K1_HB
+#define SMOT_DO_BYTE 0x200          //Digital Output Byte
+#define SMOT_AO_BYTE 0x400          //Analog Output Byte 0 Control Byte, 1 K1_LB, 2 K1_HB
+
+
+#define SCHRITTMOTOR_H
+#endif
