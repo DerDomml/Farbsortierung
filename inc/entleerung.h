7f7e33b8a3ec4e04fb380841a22a97f3ea0da8f2
spawnferkel11 <58419633+spawnferkel11@users.noreply.github.com>
2019-12-03 10:35:44 +0100

    Actor defines

--- inc/entleerung.h
+++ inc/entleerung.h
@@ -1,3 +1,6 @@
+/**
+ *  Defines fuer Sensoren
+**/
 
 #define FS_ENTLEER_ABTURM_OBEN          0x0001
 #define FS_ENTLEER_ABTURM_UNTEN         0x0002
@@ -9,6 +12,22 @@
 #define FS_ENTLEER_BO_ENDE_SENSOR       0x1000
 #define FS_ENTLEER_KRAN_OBEN            0x2000
 #define FS_ENTLEER_KRAN_UNTEN           0x4000
+
+/**
+ *  Defines fuer Aktoren
+**/
+
+#define FS_ENTLEER_ANTURM_HOCHFAHREN        0x8000
+#define FS_ENTLEER_ANTURM_RUNTERFAHREN      0x4000
+#define FS_ENTLEER_ANTURM_BAND              0x2000
+#define FS_ENTLEER_BANDLAUF_OBEN            0x0800
+#define FS_ENTLEER_GREIFARM_HOCHFAHREN      0x0200
+#define FS_ENTLEER_GREIFARM_RUNTERFAHREN    0x0100
+#define FS_ENTLEER_GREIFARM_ZU           	0x0004
+#define FS_ENTLEER_ABTURM_HOCHFAHREN        0x0040
+#define FS_ENTLEER_ABTURM_RUNTERFAHREN      0x0020
+#define FS_ENTLEER_ABTURM_BAND          	0x0010
+#define FS_ENTLEER_ZYLINDER_ROTIEREN        0x0080
 
 uint16_t CAN_received;
 uint16_t CAN_toSend;
